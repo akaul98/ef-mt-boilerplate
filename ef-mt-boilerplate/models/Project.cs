95ed389cb7d41c7518d1e374716c12518ab38034
Adarsh Kaul <adarsh.kaul@atidan.com>
2025-10-03 20:52:20 +0530

    Add Project, Tenant, and Users models
    Introduced three new classes in the `ef_mt_boilerplate.models` namespace:
    - `Project`: Represents a project with properties for ID, name, tenant, and user relationships.
    - `Tenant`: Represents a tenant with properties for ID and name.
    - `Users`: Represents a user with properties for ID, name, email, tenant, and role, along with a relationship to the `Tenant` class.
    
    These changes establish foundational models for managing projects, tenants, and users in the application.

---
+++ ef-mt-boilerplate/models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace ef_mt_boilerplate.models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public int TenantId { get; set; }
+
+        public Tenant Tenant { get; set; }
+        public int UserId { get; set; }
+        public Users User { get; set; }
+    }
+}
---
+++ ef-mt-boilerplate/models/Tenant.cs
@@ -0,0 +1,8 @@
+﻿namespace ef_mt_boilerplate.models
+{
+    public class Tenant
+    {
+        public int TenantId { get; set; }
+        public string Name { get; set; }
+    }
+}
